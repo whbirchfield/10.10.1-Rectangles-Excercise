14be1cdd9d3e6d646d7f33269e82eb9a4a27e3b8
Will B <willhbjr@gmail.com>
2021-02-24 03:00:56 +0000

    made spaces with characters F.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 let lengthOfrectangle = Number(50);
 let heightOfrectangle = Number(7);
-let squareSides = Number(5)
+let squareSides = Number(20);
+let downwardStairs = 15;
 
 function makeLine (lengthOfrectangle){
 let rectangleLength = "";
@@ -29,3 +30,38 @@ function makeSquare(sides){
 }
 console.log(makeSquare(squareSides));
 
+function makeDownwardStairs(height){
+let downwardStairs = "";
+for(let i = 0; i< height; i++){
+  downwardStairs += makeLine(i) + "\n";
+}
+return downwardStairs;
+}
+console.log(makeDownwardStairs(downwardStairs));
+
+function makeLineSpaces (numSpaces){
+let numSpacesDisplay = "";
+  for (let i=0; i<numSpaces; i++){
+    numSpacesDisplay += " ";
+  }
+  return numSpacesDisplay;
+}
+
+function makeLineChars (numChars){
+let numCharsDisplay = "";
+  for (let i=0; i<numChars; i++){
+    numCharsDisplay += "#";
+  }
+  return numCharsDisplay;
+}
+
+let numSpaces = Number(14);
+let numChars = Number (8);
+
+function makeSpaceLine (numSpaces, numChars){
+  let spaceLine = "";
+      spaceLine = makeLineSpaces(numSpaces) + makeLineChars(numChars) + makeLineSpaces(numSpaces);
+  
+  return spaceLine;
+}
+console.log(makeSpaceLine(numSpaces, numChars));
